fe8c1d490629c23a9f8e630902966fa8643263d8
José Rafael Ortega González <jose_ortega2@ucne.edu.do>
2025-10-26 19:23:32 -0400

    Reapply "Creacion de los services para el api"
    This reverts commit 4e7a3c47c2f9590b5d7a662557c47134892a1e8d.

--- RegistroJugadores/Services/IJugadoresApiService.cs
+++ RegistroJugadores/Services/IJugadoresApiService.cs
@@ -6,6 +6,6 @@ namespace RegistroJugadores.Services;
 public interface IJugadoresApiService
 {
     Task<Resource<List<JugadorResponse>>> GetJugadoresAsync();
-    Task<Resource<JugadorResponse>> GetJugadoresAsync(string nombre);
+    Task<Resource<JugadorResponse>> GetJugadoresAsync(int JugadorId);
     Task<Resource<JugadorResponse>> PostJugadores(string nombre, string email);
 }
